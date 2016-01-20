123854298d2ff72272db6941fb27699e716de7bc
Nimesh <nimeshg@navantis.com>
2016-01-20 12:52:14 +0530

    added the mvc support

--- Wizchat/Startup.cs
+++ Wizchat/Startup.cs
@@ -15,6 +15,7 @@ namespace Wizchat
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,10 +23,9 @@ namespace Wizchat
         {
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
         }
 
         // Entry point for the application.
