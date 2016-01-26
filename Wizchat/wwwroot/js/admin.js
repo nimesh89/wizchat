28c0df38d0446cd74ed4b50c371428b2916356ee
Nimesh <nimeshg@navantis.com>
2016-01-26 11:04:56 +0530

    chat system completed

--- Wizchat/wwwroot/js/admin.js
+++ Wizchat/wwwroot/js/admin.js
@@ -1,9 +1,6 @@
 ﻿angular.module("main", []).value("container", {}).run(["$http", "container", function ($http, container) {
     var proxy = $.connection.chatHub;
     container.proxy = proxy;
-    proxy.client.addNewMessage = function (from, message) {
-        alert(from + " " + message);
-    }
 
     //proxy.client.addUser = function (name) {
     //    alert(name);
@@ -17,10 +14,24 @@
     .fail(function () { console.log('Could not Connect!'); });
 }]).controller("userlist", ["$scope", "$http", "container", "$timeout", function ($scope, $http, container, $timeout) {
     $scope.users = [];
-
+    $scope.container = container;
     container.proxy.client.addUser = function (name) {
         $timeout(function () {
             $scope.users.push(name);
         }, 10, true);
+    }
+}]).controller("messagectrl", ["$scope", "$http", "container", "$timeout", function ($scope, $http, container, $timeout) {
+    var messages = [];
+
+    container.proxy.client.addNewMessage = function (from, message) {
+        $timeout(function () {
+            messages.push({ from: from, message: message });
+            $scope.messageDisplay = ($scope.messageDisplay || "") + (messages.length === 1 ? "" : "\n") + from + ":" + message;
+        }, 10, true);
+    }
+}]).controller("sendmessage", ["container", "$scope", function (container, $scope) {
+    $scope.send = function () {
+        container.proxy.server.send(container.selecteduser, "admin", $scope.message);
+        $scope.message = "";
     }
 }]);
--- Wizchat/wwwroot/js/client.js
+++ Wizchat/wwwroot/js/client.js
@@ -4,10 +4,6 @@
 
         var proxy = $.connection.chatHub;
 
-        proxy.addMessage = function (to, from, message) {
-            alert("" + from + " " + message);
-        }
-
         $.connection.hub.start()
         .done(function () { console.log('Now connected, connection ID=' + $.connection.hub.id); })
         .fail(function () { console.log('Could not Connect!'); });
@@ -25,5 +21,16 @@
     .controller("sendmessage", ["container", "$scope", function (container, $scope) {
         $scope.send = function () {
             container.proxy.server.send("admin", container.currentuser, $scope.message);
+            $scope.message = "";
+        }
+    }])
+    .controller("messagectrl", ["$scope", "$http", "container", "$timeout", function ($scope, $http, container, $timeout) {
+        var messages = [];
+
+        container.proxy.client.addNewMessage = function (from, message) {
+            $timeout(function () {
+                messages.push({ from: from, message: message });
+                $scope.messageDisplay = ($scope.messageDisplay || "") + (messages.length === 1 ? "" : "\n") + from + ":" + message;
+            }, 10, true);
         }
     }]);
